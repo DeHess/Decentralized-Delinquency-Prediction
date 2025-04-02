4926d7aa1473f7caa1f9da97547f0f36d860b08c
Tziantopoulos Alexis (tzianale) <tzianale@students.zhaw.ch>
2025-04-02 11:57:13 +0200

    Add post-processing functionality for anomaly scoring and refine pre-processing return value

---
+++ CreditScoring/post_processing.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import shap
+
+
+def anomaly_score(model, entry):
+    
+    shaptrain = pd.read_csv("Data/cs-training.csv")
+    shaptrain = shaptrain.drop(["SeriousDlqin2yrs", "Id"], axis=1)
+    background = shaptrain.iloc[np.random.choice(shaptrain.shape[0], 100, replace=False)]
+
+
+    explainer = shap.TreeExplainer(model, data=background)
+
+    shap_values = explainer.shap_values(entry)
+    print(f"This are the shap Values: {shap_values}")
+
+    def calculate_anomaly_score(instance_shap, background_shap_values):
+        bg_means = np.mean(background_shap_values, axis=0)
+        bg_stds = np.std(background_shap_values, axis=0)
+        epsilon = 1e-8
+        z_scores = (instance_shap - bg_means) / (bg_stds + epsilon)
+        abs_z_scores = np.abs(z_scores)
+        anomaly_score = np.mean(abs_z_scores)
+        return anomaly_score
+
+    instance_shap = np.array(shap_values[0])
+    background_shap_values = np.array(explainer.shap_values(background))
+
+
+    anomaly_score = calculate_anomaly_score(instance_shap, background_shap_values)
+    print("Anomaly Score:", anomaly_score)
+    return anomaly_score
--- CreditScoring/pre_processing.py
+++ CreditScoring/pre_processing.py
@@ -10,6 +10,6 @@ def pre_processing(datapoint):
     datapoint = datapoint.to_numpy()
     datapoint_np = np.nan_to_num(datapoint, nan=0.0)
     datapoint_scaled = scaler.transform(datapoint_np)
-    is_outlier = model_outlier.predict(datapoint_scaled)[0]
-    1 if is_outlier == 1 else 0
+    return model_outlier.predict(datapoint_scaled)[0]
+    
 
